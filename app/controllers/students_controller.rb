5c67f18118e5ac16b3c193aa8b6718ba7d1cf6a3
Owallah <mikesonowallah@gmail.com>
2023-06-21 23:14:14 +0300

    Completed Code along

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def search
+    student = Student.find_by(params[{ key1: "value1", key2: "value2" }])
+    render json: student
+  end
+
 end
